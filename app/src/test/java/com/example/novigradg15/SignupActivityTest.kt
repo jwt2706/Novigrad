a083ee9290d3d7af0ebcb8e1bae18050201a9dc0
jwt2706 <jwt2home@gmail.com>
2023-11-22 22:43:03 -0500

    double sided tests

--- app/src/test/java/com/example/novigradg15/SignupActivityTest.kt
+++ app/src/test/java/com/example/novigradg15/SignupActivityTest.kt
@@ -2,11 +2,20 @@ package com.example.novigradg15
 
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 class SignupActivityTest {
     @Test
     fun testBadSignupCreds() {
-        val signup = mock(SignupActivity::class.java)
-        assertEquals(false, signup.createAccount("coolman123", "bademailformat", "passwrd432", "G1", "Admin"))
+        val signupActivity = mock(SignupActivity::class.java)
+        `when`(signupActivity.createAccount("coolman123", "badEmailFormat.com", "passwrd432", "G1", "Admin")).thenReturn(false)
+        assertEquals(false, signupActivity.createAccount("coolman123", "badEmailFormat.com", "passwrd432", "G1", "Admin"))
+    }
+
+    @Test
+    fun testGoodSignupCreds() {
+        val signupActivity = mock(SignupActivity::class.java)
+        `when`(signupActivity.createAccount("user564", "user@novigrad.gov", "passwrd432", "G1", "Customer")).thenReturn(true)
+        assertEquals(true, signupActivity.createAccount("user564", "user@novigrad.gov", "passwrd432", "G1", "Customer"))
     }
 }
